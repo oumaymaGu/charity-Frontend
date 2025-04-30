9e0315a138b6f5ec5d46e7c49a561d9cbb459dc0
eya <saidi.eya@esprit.tn>
2025-04-30 10:51:40 +0100

    frontversioneya

--- src/app/back_end/navbar/navbar.component.ts
+++ src/app/back_end/navbar/navbar.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/back_end/services/don-notification.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
   username: string | null = null;
+  @Output() toggleNotifications = new EventEmitter<void>();
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    public notificationService: NotificationService
+  ) {}
 
   ngOnInit() {
     this.username = this.authService.getUsername(); // Récupérer le nom de l'utilisateur
@@ -18,5 +22,9 @@ export class NavbarComponent {
 
   logout() {
     this.authService.logout();
+  }
+
+  onToggleNotifications() {
+    this.toggleNotifications.emit();
   }
 }
